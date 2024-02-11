06eff066301b8ddc66af3a56c742ec1cda11a7d5
Nicholas Croulet <ncroulet@gmail.com>
2024-02-11 15:39:46 -0500

    first commit

---
+++ ConvoyApp/app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -0,0 +1,41 @@
+package edu.temple.convoy
+
+import android.app.DownloadManager.Request
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // local variables to store the pass + user + session key entered by the user
+        var username: String
+        var password: String
+        var sessionKey: String
+        val url = "https://kamorris.com/lab/convoy/account.php"
+
+
+
+        findViewById<Button>(R.id.accountAccessButton).setOnClickListener {
+            username = findViewById<EditText>(R.id.userEditText).text.toString()
+            password = findViewById<EditText>(R.id.passwordEditText).text.toString()
+
+            // POST request for logging in
+
+            // hashmap for the POST request
+            val params = HashMap<String,String>()
+            params["username"] = username
+            params["password"] = password
+            val requestQueue = Volley.newRequestQueue(this)
+
+            //val stringRequest = StringRequest()
+
+        }
+    }
+}
